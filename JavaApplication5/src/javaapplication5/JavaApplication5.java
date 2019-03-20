896f21d8a5de3761536c78d047d903a5b805dc02
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-03-20 23:10:49 +0530

    java10
    java10

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,17 @@
+
+package javaapplication5;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println(" +\"\"\"\"\"+\n[| 0 0 |]\n |  ^  |\n | |-| |\n +-----+"); // TODO code application logic here
+    }
+    
+}
